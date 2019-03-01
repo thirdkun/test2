2d76d9fc168457a91a278014e591000684f45464
thirdkun <thirdboya42@gmail.com>
2019-03-01 16:11:55 +0700

    ข้อ 3

--- pracex2_3.cpp
+++ pracex2_3.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
 
 using namespace std;
 char table[8][8];
@@ -22,5 +25,19 @@ void showTable(){
 }
 
 //Write definition of function randomTable() here.
+
+void randomTable(){
+		srand(time(0));	
+		for(int i=0;i<8;i++){
+		for(int j=0;j<8;j++){
+		
+			cout<< rand()%26+1<<" "; 
+		}
+		cout<<endl;
+		}
+}
+		
+
+
 
 
