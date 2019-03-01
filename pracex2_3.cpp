80a4a9cd70f06a1d3a932f08c28f74df6c6c7365
thirdkun <thirdboya42@gmail.com>
2019-03-01 14:32:42 +0700

    ข้อสอบ

---
+++ pracex2_3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+char table[8][8];
+
+void showTable();
+void randomTable();
+
+int main(){
+	srand(time(0));	
+	randomTable();
+	showTable();
+}
+
+void showTable(){
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			cout << table[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+//Write definition of function randomTable() here.
+
+
---
+++ pracex2_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Write prototype of  sg() here.
+
+int main(){
+    cout << sg(-2.5,1.1) << "\n";
+    cout << sg(1,10000) << "\n";
+    cout << sg(8.5,2.4) << "\n";
+    cout << sg(0.1,0.0001) << "\n";
+    cout << sg(10,0.007) << "\n";
+    cout << sg(1,-2) << "\n";
+    cout << sg(15,100) << "\n";
+    cout << sg(123,5432) << "\n";
+}
+
+//Write function definition of  sg() here.
